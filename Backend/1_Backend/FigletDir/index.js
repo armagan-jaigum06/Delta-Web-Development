b2b59e7964c7634675787a7987c7f0d011712660
Armagan Jaigum <armaganjaigum06@gmail.com>
2024-09-16 01:24:38 +0530

    add

---
+++ Backend/1_Backend/FigletDir/index.js
@@ -0,0 +1,10 @@
+const figlet = require("figlet");
+
+figlet("Rahul Kumar!!", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
