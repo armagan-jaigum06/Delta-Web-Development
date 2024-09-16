85b8b0ea53cb85ed31e52cab48f4c92aedeeab4e
Armagan Jaigum <armaganjaigum06@gmail.com>
2024-09-16 10:48:47 +0530

    update

---
+++ BACKEND/1_NodeJsDir/README.md
@@ -0,0 +1,7 @@
+# Backend part 1 Node.js
+
+For Project we upload only package.json folder to gate the package 
+select that folder in commad and type in terminal 
+# npm install
+
+dir > directory
---
+++ BACKEND/1_NodeJsDir/math.js
@@ -0,0 +1,20 @@
+// const sum = (a, b) => a + b;
+// const mul = (a, b) => a * b;
+// const g = 9.8;
+// const PI = 3.14;
+
+// module.exports = "hello";
+
+// let obj = {
+//   sum: sum,
+//   mul: mul,
+//   g: g,
+//   PI: PI,
+// };
+
+// module.exports = obj;
+
+// 13. by using import
+
+export const sum = (a, b) => a + b;
+export const mul = (a, b) => a * b;
